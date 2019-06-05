63fb09f286299f55ddda6a777d833b705c1e458c
Michael Brown <Vurclash@gmail.com>
2019-06-05 15:00:49 -0600

    Updated app, ran into more errors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import OptionBox from "./components/OptionBox";
+import rock from "./images/Rock.jpg";
+import paper from "./images/Paper.jpeg";
+import scissor from "./images/Scissors.jpeg"
+import optionClick from './components/OptionBox.js'
+import { Container, Header, } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+ state = { userChoice: null, compChoice: null, };
+
+ optionClick = (option) => {
+   const {userChoice} = this.state;
+   this.setState({userChoice: option});
+ };
+
+  render () {
+    return(
+      <Container style={{marginTop: "25px", }}>
+        <Header as="h1">React RPS</Header>
+        <hr />
+        <OptionBox name="rock" img={rock} optionClick={this.optionClick}/>
+        <OptionBox name="paper" img={paper} />
+        <OptionBox name="scissors" img={scissor} />
+        <hr />
+        {/* <h2 src={userChoice}></h2> */}
+      </Container>
+      
+    )
+  }
 }
 
 export default App;
